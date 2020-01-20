eed21a8f39b082e8b63f24c355bf5c30e12e1b3c
Jonathan Lathiere <lathierejonathan@gmail.com>
2020-01-20 13:33:25 +0100

    Notifying by all means

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -14,7 +14,10 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 use Twig\Environment;
 
 class ConferenceController extends AbstractController
@@ -59,7 +62,7 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{slug}", name="conference")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir)
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, NotifierInterface $notifier, string $photoDir)
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -87,9 +90,16 @@ class ConferenceController extends AbstractController
                 'permalink' => $request->getUri(),
             ];
 
-            $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
+            $reviewUrl = $this->generateUrl('review_comment', ['id' => $comment->getId()], UrlGenerator::ABSOLUTE_URL);
+            $this->bus->dispatch(new CommentMessage($comment->getId(), $reviewUrl, $context));
+
+            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation', ['browser']));
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
+        if ($form->isSubmitted()) {
+            $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
         }
 
         $offset = max(0, $request->query->getInt('offset', 0));
--- src/Message/CommentMessage.php
+++ src/Message/CommentMessage.php
@@ -5,11 +5,13 @@ namespace App\Message;
 class CommentMessage
 {
     private $id;
+    private $reviewUrl;
     private $context;
 
-    public function __construct(int $id, array $context = [])
+    public function __construct(int $id, string $reviewUrl, array $context = [])
     {
         $this->id = $id;
+        $this->reviewUrl = $reviewUrl;
         $this->context = $context;
     }
 
@@ -18,8 +20,14 @@ class CommentMessage
         return $this->id;
     }
 
+    public function getReviewUrl(): string
+    {
+        return $this->reviewUrl;
+    }
+
     public function getContext(): array
     {
         return $this->context;
     }
+
 }
